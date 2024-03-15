ff13fd0f63b44e0aca73707fb2f0df3ba25a254e
MuneerBSK <muneerbsk96@gmail.com>
2024-03-15 19:11:38 -0400

    AutoPush

--- subnets.tf
+++ subnets.tf
@@ -1,11 +1,12 @@
 # Creates Public Subnets
 resource "aws_subnet" "public_subnet" {
-  count                    = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id                   = aws_vpc.main.id
-  cidr_block               = element(var.PUBLIC_SUBNET_CIDR, count.index)
-  availability_zone        = element(var.AZ, count.index)
+  count          = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id         = aws_vpc.main.id
+  cidr_block     = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "robot-public-subnet"
+    Name = "robot-public-subnet-${count.index}"
   }
+
+  availability_zone = element(var.AZ, count.index)
 }
